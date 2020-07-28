5238bd5a5233086fcaeec517660a0cdd0eb0b88d
Long Nguyen <n.h.long.9697@gmail.com>
2020-07-28 14:04:27 +0700

    project init

---
+++ src/test/java/com/quiz/casestudy/CaseStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quiz.casestudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaseStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
