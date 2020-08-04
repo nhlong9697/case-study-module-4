9ed276b6fb49d1a6f89f8cb72393c5646a085e71
Long Nguyen <n.h.long.9697@gmail.com>
2020-08-04 14:17:48 +0700

    add language resolver

---
+++ src/main/java/com/quiz/casestudy/config/WebConfig.java
@@ -0,0 +1,41 @@
+package com.quiz.casestudy.config;
+
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.CookieLocaleResolver;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer, ApplicationContextAware {
+
+    private ApplicationContext appContext;
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        appContext = applicationContext;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        LocaleChangeInterceptor interceptor = new LocaleChangeInterceptor();
+        interceptor.setParamName("lang");
+        registry.addInterceptor(interceptor);
+    }
+    @Bean
+    public LocaleResolver localeResolver() {
+//        SessionLocaleResolver localeResolver = new SessionLocaleResolver();
+        CookieLocaleResolver localeResolver = new CookieLocaleResolver();
+        //add cookie age for window
+        localeResolver.setCookieMaxAge(1000);
+        return localeResolver;
+    }
+
+
+}
